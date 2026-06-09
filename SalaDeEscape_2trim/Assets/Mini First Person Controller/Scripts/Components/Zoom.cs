2f7337af1f63103dde1f86113faec3ab193e462e
0180001-sketch-ViinPerez <20241ctb0180001@estudantes.ifpr.edu.br>
2026-06-09 17:53:53 -0300

    Update Zoom.cs

--- SalaDeEscape_2trim/Assets/Mini First Person Controller/Scripts/Components/Zoom.cs
+++ SalaDeEscape_2trim/Assets/Mini First Person Controller/Scripts/Components/Zoom.cs
@@ -28,7 +28,7 @@ public class Zoom : MonoBehaviour
     void Update()
     {
         // Update the currentZoom and the camera's fieldOfView.
-        currentZoom += Input.mouseScrollDelta.y * sensitivity * .05f;
+        //currentZoom += Input.mouseScrollDelta.y * sensitivity * .05f;
         currentZoom = Mathf.Clamp01(currentZoom);
         camera.fieldOfView = Mathf.Lerp(defaultFOV, maxZoomFOV, currentZoom);
     }
